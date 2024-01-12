434606ef95ef2a528a96373673d0087150e3237a
GineTik <densheva18072003@gmail.com>
2024-01-12 16:25:41 +0200

    Created MainPage and implemented the functionality of adding a user

---
+++ SimpleUWPTraineeProject/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace SimpleUWPTraineeProject.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+    }
+}
--- SimpleUWPTraineeProject/ViewModels/MainViewModel.cs
+++ SimpleUWPTraineeProject/ViewModels/MainViewModel.cs
@@ -1,9 +1,45 @@
-﻿using SimpleUWPTraineeProject.ViewModels.Base;
+﻿using SimpleUWPTraineeProject.Models;
+using SimpleUWPTraineeProject.ViewModels.Base;
+using System.Collections.ObjectModel;
 
 namespace SimpleUWPTraineeProject.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
+        private string _firstName;
+        private string _secondName;
 
+        public string FirstName
+        {
+            get => _firstName;
+            set => SetField(ref _firstName, value);
+        }
+
+        public string SecondName
+        {
+            get => _secondName;
+            set => SetField(ref _secondName, value);
+        }
+
+        public ObservableCollection<User> Users { get; set; }
+
+        public RelayCommand AddUserCommand { get; set; }
+
+        public MainViewModel()
+        {
+            FirstName = "";
+            SecondName = "";
+            AddUserCommand = new RelayCommand(AddUser);
+            Users = new ObservableCollection<User>();
+        }
+
+        public void AddUser(object _)
+        {
+            Users.Add(new User
+            {
+                FirstName = FirstName,
+                SecondName = SecondName
+            });
+        }
     }
 }
